5943f58d7f505f0f3832ebba43c3a98d5b26a54b
jamiejshin <105248366+jamiejshin@users.noreply.github.com>
2022-05-09 18:12:44 -0400

    Update dvdrentalsample_staffproductivity.sql

--- dvdrentalsample_staffproductivity.sql
+++ dvdrentalsample_staffproductivity.sql
@@ -1,3 +1,10 @@
+-- This query joins two tables: payment and staff. I got this sample from a dvdrentals test database online. 
+-- I wanted to showcase how I would use JOINS and aggregate functions to identify the first and last names of all staff
+-- and count the total number of customer payments, as well as the total amount of customer payments processed by each staff.
+-- In a hypothetical scenario, I would run this query to identify the most productive staff member in terms of customer payment totals (column 'payment_totals')
+-- though depending on the business problem, 'payment_counts' could also be insightful.
+-- If there were more than two staff, I could also add 'LIMIT 3;' to the end of the code to identify the top three most productive employees.
+
 SELECT 
 	first_name, 
 	last_name, 
